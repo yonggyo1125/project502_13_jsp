764baa32fa639f1930a7a87ec20c19544e723d3a
yonggyo1125 <yonggyo00@kakao.com>
2024-06-17 08:58:29 +0900

    빈 컨테이너 수정

--- src/main/java/org/choongang/global/router/RouterService.java
+++ src/main/java/org/choongang/global/router/RouterService.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Service;
 
+import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor
 public class RouterService {
@@ -16,7 +18,7 @@ public class RouterService {
      * 컨트롤러 라우팅
      *
      */
-    public void route(HttpServletRequest req, HttpServletResponse res) throws Exception {
+    public void route(HttpServletRequest req, HttpServletResponse res) throws IOException {
         Object con = handlerMapping.search(req);
         if (con == null) { // 처리 가능한 컨트롤러를 못찾은 경우 404 응답 코드
             res.sendError(HttpServletResponse.SC_NOT_FOUND);
