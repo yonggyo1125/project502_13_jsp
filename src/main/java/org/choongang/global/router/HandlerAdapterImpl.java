db8337aa5761cb600a72607623bf9be39d320faa
yonggyo1125 <yonggyo00@kakao.com>
2024-06-18 08:52:55 +0900

    .요청 데이터 변환 처리

--- src/main/java/org/choongang/global/router/HandlerAdapterImpl.java
+++ src/main/java/org/choongang/global/router/HandlerAdapterImpl.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.choongang.global.config.annotations.RequestParam;
 import org.choongang.global.config.annotations.RestController;
 import org.choongang.global.config.annotations.Service;
 
@@ -44,33 +45,74 @@ public class HandlerAdapterImpl implements HandlerAdapter {
         /* 메서드 매개변수 의존성 주입 처리 S */
         List<Object> args = new ArrayList<>();
         for (Parameter param : method.getParameters()) {
-            Class cls = param.getType();
-
-            if (cls == HttpServletRequest.class) {
-                args.add(request);
-            } else if (cls == HttpServletResponse.class) {
-                args.add(response);
-            } else if (cls == String.class) {
-                // 문자열인 경우
-                args.add("test");
-            } else { // 기타는 setter를 체크해 보고 요청 데이터를 주입
-                for (Method _method : cls.getDeclaredMethods()) {
-                    String name = _method.getName();
-                    if (!name.startsWith("set")) continue;;
-
-                    char[] chars = name.replace("set", "").toCharArray();
-                    chars[0] = Character.toLowerCase(chars[0]);
-                    name = String.valueOf(chars);
-                    String value = request.getParameter(name);
-                    if (value == null) continue;
-
-
-                    Class clz = _method.getParameterTypes()[0];
-
+            try {
+                Class cls = param.getType();
+                String paramName = null, paramValue = null;
+                for (Annotation pa : param.getDeclaredAnnotations()) {
+                    if (pa instanceof RequestParam) {
+                        RequestParam requestParam = (RequestParam) pa;
+                        paramName = requestParam.value();
+                        break;
+                    }
                 }
-                Object arg = null;
-                args.add(arg);
-            } // endif
+
+                if (paramName != null && !paramName.isBlank()) {
+                    paramValue = request.getParameter(paramName);
+                    if (cls == int.class || cls == Integer.class || cls == long.class || cls == Long.class || cls == double.class || cls == Double.class ||  cls == float.class || cls == Float.class) {
+                        paramValue = paramValue == null || paramValue.isBlank()?"0":paramValue;
+                    }
+                }
+
+                if (cls == HttpServletRequest.class) {
+                    args.add(request);
+                } else if (cls == HttpServletResponse.class) {
+                    args.add(response);
+                } else if (cls == int.class) {
+                    args.add(Integer.parseInt(paramValue));
+                } else if (cls == Integer.class) {
+                    args.add(Integer.valueOf(paramValue));
+                } else if (cls == long.class) {
+                    args.add(Long.parseLong(paramValue));
+                } else if (cls == Long.class) {
+                    args.add(Long.valueOf(paramValue));
+                } else if (cls == float.class) {
+                    args.add(Float.parseFloat(paramValue));
+                } else if (cls == Float.class) {
+                    args.add(Float.valueOf(paramValue));
+                } else if (cls == double.class) {
+                    args.add(Double.parseDouble(paramValue));
+                } else if (cls == Double.class) {
+                    args.add(Double.valueOf(paramValue));
+                } else if (cls == String.class) {
+                    // 문자열인 경우
+                    args.add(paramValue);
+                } else {
+                    // 기타는 setter를 체크해 보고 요청 데이터를 주입
+                    // 동적 객체 생성
+                    Object paramObj = cls.getDeclaredConstructors()[0].newInstance();
+                    for (Method _method : cls.getDeclaredMethods()) {
+                        String name = _method.getName();
+                        if (!name.startsWith("set")) continue;
+                        ;
+
+                        char[] chars = name.replace("set", "").toCharArray();
+                        chars[0] = Character.toLowerCase(chars[0]);
+                        name = String.valueOf(chars);
+                        String value = request.getParameter(name);
+                        if (value == null) continue;
+
+
+                        Class clz = _method.getParameterTypes()[0];
+                        // 자료형 변환 후 메서드 호출 처리
+                        invokeMethod(paramObj,_method, value, clz);
+
+
+                    }
+                    args.add(paramObj);
+                } // endif
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
         }
         /* 메서드 매개변수 의존성 주입 처리 E */
 
@@ -98,8 +140,59 @@ public class HandlerAdapterImpl implements HandlerAdapter {
             rd.forward(request, response);
 
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
         }
         /* 요청 메서드 호출 E */
+    }
+
+    /**
+     * 자료형 변환 후 메서드 호출 처리
+     *
+     * @param paramObj
+     * @param method
+     * @param value
+     * @param clz
+     */
+    private void invokeMethod(Object paramObj, Method method, String value, Class clz) {
+        try {
+            if (clz == String.class) { // 문자열 처리
+                method.invoke(paramObj, value);
+
+                /* 기본 자료형 및 Wrapper 클래스 자료형 처리  S */
+            } else if (clz == boolean.class) {
+                method.invoke(paramObj, Boolean.parseBoolean(value));
+            } else if (clz == Boolean.class) {
+                method.invoke(paramObj, Boolean.valueOf(value));
+            } else if (clz == byte.class) {
+                method.invoke(paramObj, Byte.parseByte(value));
+            } else if (clz == Byte.class) {
+                method.invoke(paramObj, Byte.valueOf(value));
+            } else if (clz == short.class) {
+                method.invoke(paramObj, Short.parseShort(value));
+            } else if (clz == Short.class) {
+                method.invoke(paramObj, Short.valueOf(value));
+            } else if (clz == int.class) {
+                method.invoke(paramObj, Integer.parseInt(value));
+            } else if (clz == Integer.class) {
+                method.invoke(paramObj, Integer.valueOf(value));
+            } else if (clz == long.class) {
+                method.invoke(paramObj, Long.parseLong(value));
+            } else if (clz == Long.class) {
+                method.invoke(paramObj, Long.valueOf(value));
+            } else if (clz == float.class) {
+                method.invoke(paramObj, Float.parseFloat(value));
+            } else if (clz == Float.class) {
+                method.invoke(paramObj, Float.valueOf(value));
+            } else if (clz == double.class) {
+                method.invoke(paramObj, Double.parseDouble(value));
+            } else if (clz == Double.class) {
+                method.invoke(paramObj, Double.valueOf(value));
+                /* 기본 자료형 및 Wrapper 클래스 자료형 처리 E */
+            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -3,9 +3,7 @@ package org.choongang.member.controllers;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.choongang.global.config.annotations.Controller;
-import org.choongang.global.config.annotations.GetMapping;
-import org.choongang.global.config.annotations.RequestMapping;
+import org.choongang.global.config.annotations.*;
 import org.choongang.member.services.JoinService;
 
 @Controller
@@ -15,8 +13,9 @@ public class MemberController {
     private final JoinService joinService;
 
     @GetMapping("/{mode}")
-    public String join(String mode, RequestJoin form, HttpServletRequest request, HttpServletResponse response) {
-
+    public String join(@PathVariable("mode") String mode, @RequestParam("seq") int seq, RequestJoin form, HttpServletRequest request, HttpServletResponse response) {
+        System.out.printf("mode=%s, seq=%d%n", mode, seq);
+        System.out.println(form);
         return "member/join";
     }
 }
