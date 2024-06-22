c41e5e38c84fcfe069f7475479d6f3e4bb37c589
yonggyo1125 <yonggyo00@kakao.com>
2024-06-22 22:56:26 +0900

    공통 Advice 처리 중

--- src/main/java/org/choongang/global/advices/HandlerControllerAdvice.java
+++ src/main/java/org/choongang/global/advices/HandlerControllerAdvice.java
@@ -1,10 +1,36 @@
 package org.choongang.global.advices;
 
+import org.choongang.global.config.annotations.ControllerAdvice;
+import org.choongang.global.config.annotations.RestController;
+import org.choongang.global.config.annotations.RestControllerAdvice;
 import org.choongang.global.config.annotations.Service;
+import org.choongang.global.config.containers.BeanContainer;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class HandlerControllerAdvice {
     public void handle(Object controller) {
+        Class clazz = controller.getClass();
+        String pkName = clazz.getPackageName();
 
+        boolean isRest = Arrays.stream(clazz.getAnnotations()).anyMatch(a -> a instanceof RestController);
+        List<Object> advices = getControllerAdvices(isRest);
+        for (Object advice : advices) {
+            System.out.println(advice);
+        }
+
+
+    }
+
+    public List<Object> getControllerAdvices(boolean isRest) {
+
+        return BeanContainer.getInstance()
+                    .getBeans()
+                    .values()
+                    .stream()
+                    .filter(b -> Arrays.stream(b.getClass().getAnnotations()).anyMatch(a -> (!isRest && a instanceof ControllerAdvice) || (isRest && a instanceof RestControllerAdvice)))
+                    .toList();
     }
 }
--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -11,6 +11,11 @@ import org.choongang.member.services.JoinService;
 public class MemberController {
     private final JoinService joinService;
 
+    @GetMapping
+    public String index() {
+        return "member/index";
+    }
+
     @GetMapping("/{mode}/test/{num}")
     public String join(@PathVariable("mode") String mode, @RequestParam("seq") int seq, RequestJoin form,  HttpServletResponse response, @PathVariable("num") int num) {
         System.out.printf("mode=%s, seq=%d, num=%d%n", mode, seq, num);
