376969e2f25c737ea3194ea07567d35a703a5d67
yonggyo1125 <yonggyo00@kakao.com>
2024-06-25 16:36:27 +0900

    수업 소스

--- src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
+++ src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
@@ -1,9 +1,10 @@
 package org.choongang.global.advices;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.choongang.global.config.annotations.ControllerAdvice;
 import org.choongang.global.config.annotations.ModelAttribute;
-import org.choongang.global.exceptions.ExceptionHandler;
+import org.choongang.global.exceptions.*;
 
 @ControllerAdvice("org.choongang")
 public class CommonControllerAdvice {
@@ -21,7 +22,37 @@ public class CommonControllerAdvice {
      * @return
      */
     @ExceptionHandler(Exception.class)
-    public String errorHandler(Exception e, HttpServletRequest request) {
+    public String errorHandler(Exception e, HttpServletRequest request, HttpServletResponse response) {
+
+        e.printStackTrace();
+
+        if (e instanceof CommonException commonException) {
+            int status = commonException.getStatus();
+            response.setStatus(status);
+
+            StringBuffer sb = new StringBuffer(1000);
+            if (e instanceof AlertException) {
+                sb.append(String.format("alert('%s');", e.getMessage()));
+            }
+
+            if (e instanceof AlertBackException alertBackException) {
+                String target = alertBackException.getTarget();
+                sb.append(String.format("%s.history.back();", target));
+            }
+
+            if (e instanceof AlertRedirectException alertRedirectException) {
+                String target = alertRedirectException.getTarget();
+                String url = alertRedirectException.getRedirectUrl();
+
+                sb.append(String.format("%s.location.replace('%s');", target, url));
+            }
+
+            if (!sb.isEmpty()) {
+                request.setAttribute("script", sb.toString());
+                return "commons/execute_script";
+            }
+        }
+
 
         return "errors/error";
     }
