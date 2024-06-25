d86deb41601660a3031826755a9a484f692140c2
yonggyo1125 <yonggyo00@kakao.com>
2024-06-25 18:40:21 +0900

    수업 소스

--- src/test/java/org/choongang/pokemon/services/PokemonInfoService.java
+++ src/test/java/org/choongang/pokemon/services/PokemonInfoService.java
@@ -2,15 +2,20 @@ package org.choongang.pokemon.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.choongang.global.ListData;
 import org.choongang.global.config.AppConfig;
 import org.choongang.global.config.annotations.Service;
 import org.choongang.global.services.ApiRequestService;
 import org.choongang.global.services.ObjectMapperService;
 import org.choongang.pokemon.controllers.PokemonSearch;
+import org.choongang.pokemon.entities.ApiResult;
+import org.choongang.pokemon.entities.Pokemon;
 
 import java.net.http.HttpResponse;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * 포켓몬 API 목록 조회 및 상세 조회 서비스
@@ -50,14 +55,27 @@ public class PokemonInfoService {
      *
      * @param search
      */
-    public void getList(PokemonSearch search) {
+    public Optional<ListData<Pokemon>> getList(PokemonSearch search) {
         int page = search.getPage() < 1 ? 1 : search.getPage();
         int limit = search.getLimit() < 1 ? 20 : search.getLimit();
         int offset = (page - 1) * limit;
 
+        ListData<Pokemon> listData = new ListData<>();
+
         String url = String.format(apiUrl + "/pokemon?offset=%d&limit=%d", offset, limit);
 
+        HttpResponse<String> response = service.request(url);
+        if (response.statusCode() == HttpServletResponse.SC_OK) {
 
+            try {
+                ApiResult<Pokemon> apiResult = om.readValue(response.body(), new TypeReference<>() {});
+                listData.setTotal(apiResult.getCount());
+                listData.setItems(apiResult.getResults());
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
 
+        return Optional.ofNullable(listData);
     }
 }
