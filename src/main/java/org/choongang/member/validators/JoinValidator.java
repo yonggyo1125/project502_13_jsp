cf88afcde4ddd41fd5fdeaa2385a0e19f1086d5c
yonggyo1125 <yonggyo00@kakao.com>
2024-06-25 16:24:12 +0900

    수업 소스

--- src/main/java/org/choongang/member/validators/JoinValidator.java
+++ src/main/java/org/choongang/member/validators/JoinValidator.java
@@ -3,6 +3,7 @@ package org.choongang.member.validators;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Component;
+import org.choongang.global.exceptions.AlertException;
 import org.choongang.global.validators.EmailValidator;
 import org.choongang.global.validators.RequiredValidator;
 import org.choongang.global.validators.Validator;
@@ -22,10 +23,14 @@ public class JoinValidator implements Validator<RequestJoin>, RequiredValidator,
         String confirmPassword = form.getConfirmPassword();
         String userName = form.getUserName();
         boolean termsAgree = form.isTermsAgree();
-        int status = HttpServletResponse.SC_UNAUTHORIZED;
+        int status = HttpServletResponse.SC_BAD_REQUEST;
 
         /* 필수 항목 유효성 검사 S */
-
+        checkRequired(email, new AlertException("이메일을 입력하세요", status));
+        checkRequired(password, new AlertException("비밀번호를 입력하세요.", status));
+        checkRequired(confirmPassword, new AlertException("비밀번호를 확인하세요.", status));
+        checkRequired(userName, new AlertException("회원명을 입력하세요.", status));
+        checkTrue(termsAgree, new AlertException("약관에 동의해 주세요.", status));
         /* 필수 항목 유효성 검사 E */
     }
 }
