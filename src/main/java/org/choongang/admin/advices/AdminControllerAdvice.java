9754eb418d6dd919d4302c71ff0562e8f99b66a4
yonggyo1125 <yonggyo00@kakao.com>
2024-06-26 15:38:20 +0900

    수업 소스

---
+++ src/main/java/org/choongang/admin/advices/AdminControllerAdvice.java
@@ -0,0 +1,7 @@
+package org.choongang.admin.advices;
+
+import org.choongang.global.config.annotations.ControllerAdvice;
+
+@ControllerAdvice("org.choongang.admin")
+public class AdminControllerAdvice {
+}
