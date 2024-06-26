116cbde721bc4ee43927fa95993fc391f3b9bdc7
yonggyo1125 <yonggyo00@kakao.com>
2024-06-26 15:44:10 +0900

    수업 소스

--- src/main/java/org/choongang/admin/advices/AdminControllerAdvice.java
+++ src/main/java/org/choongang/admin/advices/AdminControllerAdvice.java
@@ -1,7 +1,14 @@
 package org.choongang.admin.advices;
 
 import org.choongang.global.config.annotations.ControllerAdvice;
+import org.choongang.global.config.annotations.ModelAttribute;
 
 @ControllerAdvice("org.choongang.admin")
 public class AdminControllerAdvice {
+
+    @ModelAttribute
+    public String testValue() {
+        System.out.println("testValue!!");
+        return "test";
+    }
 }
