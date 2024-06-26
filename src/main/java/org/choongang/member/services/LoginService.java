f5dec2710610c4baf076878dc9631d94140167e1
yonggyo1125 <yonggyo00@kakao.com>
2024-06-26 10:43:58 +0900

    수업 소스

---
+++ src/main/java/org/choongang/member/services/LoginService.java
@@ -0,0 +1,14 @@
+package org.choongang.member.services;
+
+import lombok.RequiredArgsConstructor;
+import org.choongang.global.config.annotations.Service;
+import org.choongang.member.controllers.RequestLogin;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    public void process(RequestLogin form) {
+
+    }
+}
