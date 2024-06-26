aaa750bad219eea28183136380223e55a0ae7157
yonggyo1125 <yonggyo00@kakao.com>
2024-06-26 09:40:05 +0900

    수업 소스

--- src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
+++ src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
@@ -51,6 +51,9 @@ public class CommonControllerAdvice {
                 request.setAttribute("script", sb.toString());
                 return "commons/execute_script";
             }
+        } else {
+            // CommonException으로 정의한 예외가 아닌 경우 - 응답 코드 500
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
 
 
