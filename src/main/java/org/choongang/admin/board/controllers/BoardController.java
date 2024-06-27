ad2a376edd04de6dd16014f1f423b0f25c847e11
yonggyo1125 <yonggyo00@kakao.com>
2024-06-27 21:11:50 +0900

    수업 소스

--- src/main/java/org/choongang/admin/board/controllers/BoardController.java
+++ src/main/java/org/choongang/admin/board/controllers/BoardController.java
@@ -2,8 +2,12 @@ package org.choongang.admin.board.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.choongang.board.entities.Board;
+import org.choongang.board.services.config.BoardConfigInfoService;
 import org.choongang.board.services.config.BoardConfigSaveService;
 import org.choongang.global.config.annotations.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,11 +15,15 @@ import org.choongang.global.config.annotations.*;
 public class BoardController {
 
     private final BoardConfigSaveService saveService;
+    private final BoardConfigInfoService infoService;
     private final HttpServletRequest request;
 
     // 게시판 목록
     @GetMapping
     public String boardList() {
+
+        List<Board> items = infoService.getList();
+        request.setAttribute("items", items);
 
         return "admin/board/index";
     }
@@ -32,6 +40,9 @@ public class BoardController {
     // 게시판 수정
     @GetMapping("/update/{bId}")
     public String update(@PathVariable("bId") String bId) {
+
+        RequestBoard form = infoService.getForm(bId);
+        request.setAttribute("data", form);
 
         return "admin/board/update";
     }
