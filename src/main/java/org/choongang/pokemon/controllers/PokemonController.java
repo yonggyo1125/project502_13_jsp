a5fc6f2eabd913d885dd78d38517407b46fc6ca6
yonggyo1125 <yonggyo00@kakao.com>
2024-06-27 14:54:40 +0900

    수업 소스

--- src/main/java/org/choongang/pokemon/controllers/PokemonController.java
+++ src/main/java/org/choongang/pokemon/controllers/PokemonController.java
@@ -2,20 +2,40 @@ package org.choongang.pokemon.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.choongang.global.ListData;
+import org.choongang.global.Pagination;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.RequestMapping;
+import org.choongang.pokemon.entities.PokemonDetail;
+import org.choongang.pokemon.services.PokemonInfoService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/pokemon")
 @RequiredArgsConstructor
 public class PokemonController {
+
+    private final PokemonInfoService infoService;
     private final HttpServletRequest request;
 
     @GetMapping
-    public String index() {
-        String uri = request.getRequestURI();
-        System.out.println(uri);
+    public String index(PokemonSearch search) {
+        commonProcess();
+
+        ListData<PokemonDetail> listData = infoService.getList(search);
+        List<PokemonDetail> items = listData.getItems();
+        Pagination pagination = listData.getPagination();
+
+        request.setAttribute("items", items);
+        request.setAttribute("pagination", pagination);
+
         return "pokemon/index";
+    }
+
+    private void commonProcess() {
+        request.setAttribute("addCss", new String[] {"pokemon/style"});
+        request.setAttribute("addScript", List.of("pokemon/wishlist"));
     }
 }
