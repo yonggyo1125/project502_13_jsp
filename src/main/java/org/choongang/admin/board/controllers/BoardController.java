624e3cc3006c5285b6a24f878ad98f00a9db5f65
yonggyo1125 <yonggyo00@kakao.com>
2024-06-27 20:07:49 +0900

    수업 소스

--- src/main/java/org/choongang/admin/board/controllers/BoardController.java
+++ src/main/java/org/choongang/admin/board/controllers/BoardController.java
@@ -1,12 +1,20 @@
 package org.choongang.admin.board.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.choongang.board.services.config.BoardConfigSaveService;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
+import org.choongang.global.config.annotations.PostMapping;
 import org.choongang.global.config.annotations.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/admin/board")
 public class BoardController {
+
+    private final BoardConfigSaveService saveService;
+    private final HttpServletRequest request;
 
     // 게시판 목록
     @GetMapping
@@ -20,5 +28,17 @@ public class BoardController {
     public String register() {
 
         return "admin/board/register";
+    }
+
+    @PostMapping("/save")
+    public String save(RequestBoard form) {
+
+        saveService.process(form);
+
+        String url = request.getContextPath() + "/admin/board";
+        String script = String.format("parent.location.replace('%s');", url);
+
+        request.setAttribute("script", script);
+        return "commons/execute_script";
     }
 }
