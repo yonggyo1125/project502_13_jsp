6cca8a2b75bda5bc42945be392e1ef8459d0b396
yonggyo1125 <yonggyo00@kakao.com>
2024-06-28 10:48:44 +0900

    수업 소스

--- src/main/java/org/choongang/board/controllers/BoardController.java
+++ src/main/java/org/choongang/board/controllers/BoardController.java
@@ -1,13 +1,22 @@
 package org.choongang.board.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.choongang.board.entities.Board;
+import org.choongang.board.exceptions.BoardConfigNotFoundException;
+import org.choongang.board.services.config.BoardConfigInfoService;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.PathVariable;
 import org.choongang.global.config.annotations.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+
+    private final BoardConfigInfoService configInfoService;
+    private final HttpServletRequest request;
 
     @GetMapping("/list/{bId}")
     public String list(@PathVariable("bId") String bId) {
@@ -31,5 +40,16 @@ public class BoardController {
     public String update(@PathVariable("seq") long seq) {
 
         return "board/update";
+    }
+
+    /**
+     * 모든 요청 처리 메서드에 공통 처리 부분
+     *
+     * @param bId : 게시판 설정 유지 / 게시판 설정 X -> 게시판이 없음(BoardConfigNotFoundException)
+     */
+    private void commonProcess(String bId) {
+        Board board = configInfoService.get(bId).orElseThrow(BoardConfigNotFoundException::new);
+
+
     }
 }
