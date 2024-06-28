29ec1c6b2c42fdb5de8b6dc8b8e0773daa3d41cf
yonggyo1125 <yonggyo00@kakao.com>
2024-06-28 10:17:20 +0900

    수업 소스

---
+++ src/main/java/org/choongang/board/controllers/BoardController.java
@@ -0,0 +1,23 @@
+package org.choongang.board.controllers;
+
+import org.choongang.global.config.annotations.Controller;
+import org.choongang.global.config.annotations.GetMapping;
+import org.choongang.global.config.annotations.PathVariable;
+import org.choongang.global.config.annotations.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @GetMapping("/list/{bId}")
+    public String list(@PathVariable("bId") String bId) {
+
+        return "board/list";
+    }
+
+    @GetMapping("/view/{seq}")
+    public String view(@PathVariable("seq") long seq) {
+
+        return "board/view";
+    }
+}
