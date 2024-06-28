f8acb498971096a317a951d79badd30acabd2691
yonggyo1125 <yonggyo00@kakao.com>
2024-06-28 18:38:56 +0900

    수업 소스

--- src/main/java/org/choongang/board/controllers/BoardController.java
+++ src/main/java/org/choongang/board/controllers/BoardController.java
@@ -10,6 +10,10 @@ import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.PathVariable;
 import org.choongang.global.config.annotations.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board")
@@ -20,7 +24,7 @@ public class BoardController {
 
     @GetMapping("/list/{bId}")
     public String list(@PathVariable("bId") String bId) {
-        commonProcess(bId);
+        commonProcess(bId, "list");
 
         return "board/list";
     }
@@ -33,7 +37,7 @@ public class BoardController {
 
     @GetMapping("/write/{bId}")
     public String write(@PathVariable("bId") String bId) {
-        commonProcess(bId);
+        commonProcess(bId, "write");
 
         return "board/write";
     }
@@ -48,10 +52,34 @@ public class BoardController {
      * 모든 요청 처리 메서드에 공통 처리 부분
      *
      * @param bId : 게시판 설정 유지 / 게시판 설정 X -> 게시판이 없음(BoardConfigNotFoundException)
+     * @param mode : 처리 모드 - write, update, list, view
      */
-    private void commonProcess(String bId) {
+    private void commonProcess(String bId, String mode) {
         Board board = configInfoService.get(bId).orElseThrow(BoardConfigNotFoundException::new);
 
+        // mode가 null이면 write로 기본값 설정
+        mode = Objects.requireNonNullElse(mode, "write");
+
+        List<String> addCss = new ArrayList<>();
+        List<String> addScript = new ArrayList<>();
+
+        addCss.add("board/style"); // 모든 게시판의 공통 스타일
+
+        if (mode.equals("write") || mode.equals("update")) { // 쓰기, 수정
+            addCss.add("board/form");
+            addScript.add("ckeditor5/ckeditor5");
+            addScript.add("board/form");
+
+        } else if (mode.equals("list")) { // 목록
+            addCss.add("board/list");
+
+        } else if (mode.equals("view")) { // 글보기
+            addCss.add("board/view");
+
+        }
+
         request.setAttribute("board", board);
+        request.setAttribute("addCss", addCss);
+        request.setAttribute("addScript", addScript);
     }
 }
