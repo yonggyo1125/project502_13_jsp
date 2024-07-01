c883e434601d9eefe2f4f1b6c1ddd5fbdf5868ef
yonggyo1125 <yonggyo00@kakao.com>
2024-07-01 12:39:57 +0900

    수업 소스

--- src/main/java/org/choongang/mypage/controllers/MypageController.java
+++ src/main/java/org/choongang/mypage/controllers/MypageController.java
@@ -1,13 +1,19 @@
 package org.choongang.mypage.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.PostMapping;
 import org.choongang.global.config.annotations.RequestMapping;
+import org.choongang.mypage.services.ProfileService;
 
 @Controller
 @RequestMapping("/mypage")
+@RequiredArgsConstructor
 public class MypageController {
+
+    private final ProfileService profileService;
 
     /**
      * 마이페이지 메인
@@ -35,7 +41,14 @@ public class MypageController {
      * @return
      */
     @PostMapping("/info")
-    public String infoPs() {
+    public String infoPs(RequestProfile form, HttpServletRequest request) {
+
+        profileService.update(form);
+
+        String url = request.getContextPath() + "/mypage";
+        String script = String.format("script", "parent.location.replace('%s');", url);
+
+        request.setAttribute("script", script);
 
         return "commons/execute_script";
     }
