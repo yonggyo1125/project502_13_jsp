2122cc3657d360322fac30d578be86847cd8fb47
yonggyo1125 <yonggyo00@kakao.com>
2024-07-02 16:37:45 +0900

    수업 소스

--- src/main/java/org/choongang/pokemon/game/controllers/PokemonGameController.java
+++ src/main/java/org/choongang/pokemon/game/controllers/PokemonGameController.java
@@ -1,15 +1,26 @@
 package org.choongang.pokemon.game.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.PostMapping;
 import org.choongang.global.config.annotations.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/pokemon/game")
 @RequiredArgsConstructor
 public class PokemonGameController {
+
+    private final HttpServletRequest request;
+
+    @GetMapping
+    public String index() {
+
+        return "redirect:/pokemon/game/step1";
+    }
 
     /**
      * 1단계 : 카드 선택
@@ -19,13 +30,19 @@ public class PokemonGameController {
      */
     @GetMapping("/step1")
     public String step1() {
+        commonProcess();
 
         return "pokemon/game/step1";
     }
 
     @PostMapping("/step2")
     public String step2() {
+        commonProcess();
 
         return "pokemon/game/step2";
+    }
+
+    private void commonProcess() {
+        request.setAttribute("addCss", List.of("pokemon/game"));
     }
 }
