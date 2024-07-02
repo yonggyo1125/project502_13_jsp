e949b31b08926276816a350245cb106da6495f48
yonggyo1125 <yonggyo00@kakao.com>
2024-07-02 11:50:49 +0900

    수업 소스

--- src/main/java/org/choongang/global/config/MapperProxyHandler.java
+++ src/main/java/org/choongang/global/config/MapperProxyHandler.java
@@ -9,11 +9,23 @@ public class MapperProxyHandler implements InvocationHandler {
     public MapperProxyHandler(Class clz) {
         this.clz = clz;
     }
+    private Object obj;
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        Object obj = DBConn.getSession().getMapper(clz);
+
+
+        if (obj == null) {
+            obj = DBConn.getSession().getMapper(clz);
+        }
+
         Object result = method.invoke(obj, args);
+
+        // get으로 시작하는 메서드가 아닌 경우는 sqlSession 객체 갱신
+        if (!method.getName().startsWith("get")) {
+            obj = null;
+        }
+
         return result;
     }
 }
