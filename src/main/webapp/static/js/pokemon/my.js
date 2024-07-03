f4c223f18227c0a46ec6040cfa442a5d4bad35e6
yonggyo1125 <yonggyo00@kakao.com>
2024-07-03 10:21:54 +0900

    수업 소스

--- src/main/webapp/static/js/pokemon/my.js
+++ src/main/webapp/static/js/pokemon/my.js
@@ -1,10 +1,17 @@
 window.addEventListener("DOMContentLoaded", function() {
     const rootUrl = document.querySelector("meta[name='rootUrl']").content;
-    const redirectUrl = `${location.pathName}${location.search}`;
+    const redirectUrl = `${location.pathname}${location.search}`;
 
     const items = document.getElementsByClassName("my-pokemon");
     for (const el of items) {
         el.addEventListener("click", function() {
+            const classList = el.classList;
+            if(classList.contains("guest")) { // 비회원으로 클릭한 경우
+                // 로그인 페이지로 이동
+                location.href= rootUrl + "member/login?redirectUrl=" + redirectUrl;
+                return;
+            }
+
 
         });
     }
