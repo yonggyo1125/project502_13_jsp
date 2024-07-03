d2b15548f5bf8f884011cb76951c75092ef1345a
yonggyo1125 <yonggyo00@kakao.com>
2024-07-03 14:17:30 +0900

    수업 소스

--- src/test/java/org/choongang/file/mappers/MapperTest.java
+++ src/test/java/org/choongang/file/mappers/MapperTest.java
@@ -1,5 +1,7 @@
 package org.choongang.file.mappers;
 
+import org.apache.ibatis.session.SqlSession;
+import org.choongang.file.entities.FileInfo;
 import org.choongang.global.config.DBConn;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,10 +11,13 @@ import java.util.UUID;
 public class MapperTest {
 
     private FileInfoMapper mapper;
-
+    private SqlSession session;
     @BeforeEach
     void init() {
-        mapper = DBConn.getSession().getMapper(FileInfoMapper.class);
+        session = DBConn.getSession();
+
+
+        mapper = session.getMapper(FileInfoMapper.class);
     }
 
     @Test
@@ -22,7 +27,16 @@ public class MapperTest {
         String location2 = "loc2";
 
         for (int i = 0; i < 10; i++) {
-
+            FileInfo item = FileInfo.builder()
+                    .gid(gid)
+                    .location(i % 2 == 0 ? location1 : location2)
+                    .fileName("파일" + i + ".png")
+                    .extension(".png")
+                    .contentType("image/png")
+                    .done(i % 2)
+                    .build();
+            mapper.register(item);
+            session.clearCache();
         }
     }
 }
